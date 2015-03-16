940411c4f4f5c699a0b56f9dbbc8d47457c1d74d
dcuerdogarcia <dcuerdogarcia@eniac10.danielcastelao.org>
2015-03-16 10:02:06 +0100

    Commit segundo: Buscar un tweet

--- src/examencodtwitter/ExamenCodTwitter.java
+++ src/examencodtwitter/ExamenCodTwitter.java
@@ -17,10 +17,32 @@ public class ExamenCodTwitter {
     public static void main(String[] args) {
         int op;
         Metodos obx = new Metodos();
-        obx.timeLine();
+      
+        do {
+            op = Integer.parseInt(JOptionPane.showInputDialog("opciones: \n1 --> TimeLine\n2 --> Buscar\n3 -->  Exit"));
+            switch (op) {
+                case 1:
+                    obx.timeLine();
+                    break;
+                case 2:
+                    obx.buscar();
+                    break;
+                //case 3:
+                    //obx.publicar;
+                   // break;
+                case 3:
+                    System.exit(0);
+                default:
+                    System.exit(0);
+            }
+        } while (op != 0 && op != 4);
+    }
+}
+
         
             
         
-    }
     
-}
+    
+    
+
